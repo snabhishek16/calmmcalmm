c66c617789d181fc6d51b96c5cdcafb7d770b051
abhishek <snabhishek16@gmail.com>
2018-03-26 18:49:56 +0530

    functionalities working

--- home/views.py
+++ home/views.py
@@ -34,15 +34,21 @@ def home_page(request):
 
 def product_category(request, name=None):
     product_list = Products.objects.all()
+    product_latest = Products.objects.filter(posted_on__lte=timezone.now()).order_by('-id')[:4]
     cat = Category.objects.all()
+    blog = Home_blog.objects.all()
     this_cat = get_object_or_404(cat, name=name)
     posts_cat = Products.objects.filter(category=this_cat.id)
 
+
+
     context = {
-        'product_list': product_list,
-        'posts_cat': posts_cat,
+        'product_list': posts_cat,
+        'product_latest': product_latest,
         'cat': cat,
+        'blog': blog,
         'current_category': name,
+
     }
     return render(request, 'home/index.html', context)
 
