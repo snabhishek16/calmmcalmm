7fba745e77170fe64d16a45e81e4ca766a41ec6f
abhisheksn <snabhishek16@gmail.com>
2018-04-02 19:01:37 +0530

    sliding added

--- home/models.py
+++ home/models.py
@@ -18,12 +18,12 @@ class Gender_category(models.Model):
 
 class Products(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField(max_length=2000)
+    # content = models.TextField(max_length=2000)
     image = models.FileField(upload_to='products/%Y/%m/%d/')
     category = models.ForeignKey(Category, null=True)
     gender_category = models.ForeignKey(Gender_category, null=True)
-    original_price = models.DecimalField( max_digits=10, decimal_places=2)
-    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    # original_price = models.DecimalField( max_digits=10, decimal_places=2)
+    # selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     posted_on = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
 
